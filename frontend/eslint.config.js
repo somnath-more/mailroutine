69b4f886aa20c385eb07aeca7d1f6073089df5f7
somnath.more@corp.vmedulife.com-at-445466614069 <somnath.more@corp.vmedulife.com>
2026-09-15 13:20:00 +0530

    chore: frontend scaffolding

---
+++ frontend/eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['coverage', 'dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: globals.browser,
+    },
+  },
+)
